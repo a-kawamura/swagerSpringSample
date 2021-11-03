9f57fcd6769daa6e754ef457c69f7028a134d2dd
Akihiro Kawamura <a-kawamura@nyc.odn.ne.jp>
2021-11-03 10:55:25 +0900

    first commit

---
+++ src/main/java/com/example/swaggerSpringSample/SwaggerSpringSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggerSpringSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringSampleApplication.class, args);
+	}
+
+}
