bb250faaac37e69e9dbaed1698bcbf379119fff1
Akihiro Kawamura <a-kawamura@nyc.odn.ne.jp>
2021-11-21 10:30:44 +0900

    implement springfox

---
+++ src/main/java/com/example/swaggerSpringSample/Greeting.java
@@ -0,0 +1,20 @@
+package com.example.swaggerSpringSample;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
